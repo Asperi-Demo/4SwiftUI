8b39777ed64e28c02a3853bcb49c49440382e949
Asperi <asperi.2791@gmail.com>
2020-03-14 14:23:46 +0200

    - Added demo of window tint based on .hostingWindow environment key

---
+++ DemoWindowTint/DemoWindowTint/HostingWindowEnvironment.swift
@@ -0,0 +1,15 @@
+// DISCLAIMER: For DEMO purpose only. Created by Asperi on 14.03.2020.
+
+import SwiftUI
+
+struct HostingWindowEnvironment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HostingWindowEnvironment_Previews: PreviewProvider {
+    static var previews: some View {
+        HostingWindowEnvironment()
+    }
+}
