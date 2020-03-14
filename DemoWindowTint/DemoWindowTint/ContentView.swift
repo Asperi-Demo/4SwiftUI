6f013fab9ca96c58e1ab5bc370d87f5b2b281155
Asperi <kostia@Kostias-MBP.Dlink>
2020-03-14 13:58:51 +0200

    Initial project for DemoWindowTint

---
+++ DemoWindowTint/DemoWindowTint/ContentView.swift
@@ -0,0 +1,15 @@
+// DISCLAIMER: For DEMO purpose only. Created by Asperi on 14.03.2020.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
