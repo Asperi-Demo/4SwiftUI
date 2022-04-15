0cff5fa319abb9236b64165f967e41ff7b681c74
Asperi <asperi.2791@gmail.com>
2022-04-15 09:49:32 +0300

    Added helper extension for Button creation with simplified interface

---
+++ PlayOn_iOS/PlayOn_iOS/Extensions/Button+.swift
@@ -0,0 +1,28 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+extension Button {
+
+	/// A helper constructor for a Button with simplified interface
+	///
+	/// Example:
+	/// ```
+	/// Button("Button", systemImage: "person") {
+	///   print("pressed")
+	/// }
+	/// ```
+	/// - Parameters:
+	///   - title: A title of button (localizable)
+	///   - systemImage: A name of SF symbol
+	///   - action: A button action
+	init(_ title: LocalizedStringKey, systemImage: String, action: @escaping () -> Void) where Label == SwiftUI.Label<Text, Image> {
+		self.init(action: action, label: {
+			SwiftUI.Label(title, systemImage: systemImage)
+		})
+	}
+}
