2ccb79f52dbc64f248c8e1ebfbd316d39650a509
Asperi <asperi.2791@gmail.com>
2022-04-16 16:36:14 +0300

    SwiftUI: how to detect when List is scrolled?

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestDetectListBeginToScroll.swift
@@ -0,0 +1,29 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestDetectListBeginToScroll: View {
+	@State private var text = ""
+	var body: some View {
+		VStack {
+			TextField("Search", text: $text)
+			List(0..<100) {
+			  Text("Item \($0)")
+			}
+			.simultaneousGesture(DragGesture().onChanged({ _ in
+				 // if keyboard is opened then hide it
+				print(">> Got it!")
+			}))
+		}
+	}
+}
+
+struct TestDetectListBeginToScroll_Previews: PreviewProvider {
+	static var previews: some View {
+		TestDetectListBeginToScroll()
+	}
+}
