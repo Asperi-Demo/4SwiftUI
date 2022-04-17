8ed2c6530dd03ed44993369295f06029e7a686a6
Asperi <asperi.2791@gmail.com>
2022-04-17 09:30:49 +0300

    Button style with state

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestFixedStateButtonStyle.swift
@@ -0,0 +1,36 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestFixedStateButtonStyle: View {
+	var body: some View {
+		Button("Demo") {}
+			.buttonStyle(MyButtonStyle())
+	}
+
+	struct MyButtonStyle: ButtonStyle {
+		@State private var flag = false
+		func makeBody(configuration: Configuration) -> some View {
+			configuration.label
+				.padding()
+				.foregroundColor(flag ? Color.white : Color.green)
+				.background(flag ? Color.red : Color.clear)
+				.onChange(of: configuration.isPressed) {
+					if $0 {
+						flag.toggle()
+					}
+				}
+		}
+	}
+
+}
+
+struct TestFixedStateButtonStyle_Previews: PreviewProvider {
+	static var previews: some View {
+		TestFixedStateButtonStyle()
+	}
+}
