d464e85bd283ff6097c1c21f56d53b99f48cd411
Asperi <asperi.2791@gmail.com>
2022-04-17 08:33:40 +0300

    Sending preference key when publisher/binding emits value

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestViewModelViaViewPreference.swift
@@ -0,0 +1,48 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestViewModelViaViewPreference: View {
+	@State private var childValue: String?
+	var body: some View {
+		VStack {
+			ChildView()
+			Text("Received: \(childValue ?? "none")")
+		}
+		.onPreferenceChange(NewValueKey.self) {
+			childValue = $0
+		}
+	}
+
+	struct ChildView: View {
+		@StateObject var vm = SomeViewModel()
+
+		var body: some View {
+			Button("Generate") {
+				vm.value = String(Int.random(in: 0...9))
+			}
+			.preference(key: NewValueKey.self, value: vm.value)
+		}
+	}
+
+	struct NewValueKey: PreferenceKey {
+		static var defaultValue: String? = nil
+		static func reduce(value: inout String?, nextValue: () -> String?) {  // << fix !!
+			value = nextValue()
+		}
+	}
+
+	public class SomeViewModel: ObservableObject {
+		@Published public var value: String?
+	}
+}
+
+struct TestViewModelViaViewPreference_Previews: PreviewProvider {
+	static var previews: some View {
+		TestViewModelViaViewPreference()
+	}
+}
