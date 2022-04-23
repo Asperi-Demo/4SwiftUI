c9b623bfc23344cee917a74d27a7eefeec29f356
Asperi <asperi.2791@gmail.com>
2022-04-23 14:36:46 +0300

    Restoring minimized window from Dock

--- PlayOn_macOS/PlayOn_macOS/PlayOn_macOSApp.swift
+++ PlayOn_macOS/PlayOn_macOS/PlayOn_macOSApp.swift
@@ -8,10 +8,21 @@ import SwiftUI
 
 @main
 struct PlayOn_macOSApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
             	.frame(width: 640, height: 480)
         }
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationDidBecomeActive(_ notification: Notification) {
+		// Restore first minimized window if app became active and no one window
+		// is visible
+		if NSApp.windows.compactMap({ $0.isVisible ? Optional(true) : nil }).isEmpty {
+			 NSApp.windows.first?.makeKeyAndOrderFront(self)
+		}
+	}
 }
