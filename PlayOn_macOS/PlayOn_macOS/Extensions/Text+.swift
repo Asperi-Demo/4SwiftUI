f8c99d8addb1f0ceb82e2ab8a64545a769aa473e
Asperi <asperi.2791@gmail.com>
2022-04-26 16:17:18 +0300

    Extension for Text for auto-search translation in alternate table

---
+++ PlayOn_macOS/PlayOn_macOS/Extensions/Text+.swift
@@ -0,0 +1,20 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+extension Text {
+	public init<S>(looking text: S, fallback table: String? = nil) where S : StringProtocol {
+		let text = String(text)
+		var translated = NSLocalizedString(text, comment: "")
+		if translated == text, let table = table {
+			translated = NSLocalizedString(text, tableName: table,
+				bundle: .main, value: text, comment: "")
+		}
+		// already translated, so avoid search again
+		self.init(verbatim: translated)
+	}
+}
