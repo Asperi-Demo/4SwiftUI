a5742f27c0b8a5a5d3ec953d9a35a164ba533d9e
Asperi <asperi.2791@gmail.com>
2022-04-26 15:54:32 +0300

    Focus color effect in TextField

---
+++ PlayOn_macOS/PlayOn_macOS/Findings/TestBackgroundOfFocusedTextField.swift
@@ -0,0 +1,69 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestBackgroundOfFocusedTextField: View {
+	@State private var text = ""
+
+	var body: some View {
+		VStack {
+			FindNavigatorSearchBar(
+				title: "placeholder",
+				text: $text
+			)
+			TextField("Standard TextField", text: .constant(""))
+				.padding(.vertical)
+		}
+		.padding()
+	}
+
+	struct FindNavigatorSearchBar: View {
+		let title: String
+
+		@Binding var text: String
+		@FocusState private var focused: Bool
+		
+		var body: some View {
+			HStack {
+				Image(systemName: "magnifyingglass")
+					.foregroundColor(Color(nsColor: .secondaryLabelColor))
+				textField
+				if !text.isEmpty { clearButton }
+			}
+			.padding(.horizontal, 5)
+			.padding(.vertical, 3)
+			.background(focused ? .black : .clear)
+			.clipShape(RoundedRectangle(cornerRadius: 4))
+			.overlay(RoundedRectangle(cornerRadius: 4).stroke(Color.gray, lineWidth: 0.5).cornerRadius(4)
+			)
+		}
+
+		private var textField: some View {
+			TextField(title, text: $text)
+				.disableAutocorrection(true)
+				.textFieldStyle(PlainTextFieldStyle())
+				.focused($focused)
+		}
+
+		private var clearButton: some View {
+			Button {
+				self.text = ""
+				// side-effect here if needed
+			} label: {
+				Image(systemName: "xmark.circle.fill")
+			}
+			.foregroundColor(.secondary)
+			.buttonStyle(PlainButtonStyle())
+		}
+	}
+}
+
+struct TestBackgroundOfFocusedTextField_Previews: PreviewProvider {
+	static var previews: some View {
+		TestBackgroundOfFocusedTextField()
+	}
+}
