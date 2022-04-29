e25e0dd63f48b1d0668bf56d0fa711263b141e82
Asperi <asperi.2791@gmail.com>
2022-04-29 08:13:24 +0300

    Experiments with custom shape style

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestCustomShapeStyle.swift
@@ -0,0 +1,42 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestCustomShapeStyle: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		var body: some View {
+			Circle()
+				.fill(RedView())
+				.aspectRatio(contentMode: .fit)
+				.border(.green)
+		}
+	}
+
+	struct RedView: ShapeStyle {
+		public static func _makeView<S>(view: SwiftUI._GraphValue<SwiftUI._ShapeView<S, SwiftUI.Color>>, inputs: SwiftUI._ViewInputs) -> SwiftUI._ViewOutputs where S : SwiftUI.Shape {
+			Color._makeView(view: view, inputs: inputs)
+		}
+
+		public func _apply(to shape: inout SwiftUI._ShapeStyle_Shape) {
+			Color.red._apply(to: &shape)
+		}
+
+		public static func _apply(to type: inout SwiftUI._ShapeStyle_ShapeType) {
+			Color._apply(to: &type)
+		}
+	}
+}
+
+struct TestCustomShapeStyle_Previews: PreviewProvider {
+	static var previews: some View {
+		TestCustomShapeStyle()
+	}
+}
