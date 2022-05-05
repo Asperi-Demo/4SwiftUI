bfefc804ec2c120d1eaec827ebb0f2f28136c3c0
Asperi <asperi.2791@gmail.com>
2022-05-05 20:11:29 +0300

    Added removing focus on scroll

--- PlayOn_iOS/PlayOn_iOS/Findings/TestDetectListBeginToScroll.swift
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestDetectListBeginToScroll.swift
@@ -8,15 +8,18 @@ import SwiftUI
 
 struct TestDetectListBeginToScroll: View {
 	@State private var text = ""
+	@FocusState var active: Bool
 	var body: some View {
 		VStack {
 			TextField("Search", text: $text)
+				.focused($active)
 			List(0..<100) {
 			  Text("Item \($0)")
 			}
 			.simultaneousGesture(DragGesture().onChanged({ _ in
 				 // if keyboard is opened then hide it
 				print(">> Got it!")
+				active = false
 			}))
 		}
 	}
