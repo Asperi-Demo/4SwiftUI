4ba4f19c73e62a904124c3a3829dca311efb7650
Asperi <asperi.2791@gmail.com>
2022-05-06 17:21:51 +0300

    Test image rect fetch

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestImageRectCornersGet.swift
@@ -0,0 +1,32 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestImageRectCornersGet: View {
+	var body: some View {
+		ZStack {
+			Image("picture")
+				.resizable()
+				.scaledToFit()
+
+				.background(GeometryReader {
+					Color.clear.preference(key: ViewRectKey.self,
+												  value: [$0.frame(in: .global)])
+				})
+				.onPreferenceChange(ViewRectKey.self) { rects in
+					print(rects.first ?? .zero)
+					// calculate here !!
+				}
+		}.edgesIgnoringSafeArea(.all)
+	}
+}
+
+struct TestImageRectCornersGet_Previews: PreviewProvider {
+	static var previews: some View {
+		TestImageRectCornersGet()
+	}
+}
