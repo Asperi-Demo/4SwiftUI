a404c3a2f62accb7d2b62b13db259d698cfb6a9c
Asperi <asperi.2791@gmail.com>
2022-05-07 19:30:38 +0300

    Added demo to configure minute interval for DatePicker

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestDatePickerMinuteInterval.swift
@@ -0,0 +1,39 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestDatePickerMinuteInterval: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		@State private var selectedDate = Date()
+		
+		init() {
+			UIDatePicker.appearance().minuteInterval = 15
+		}
+		
+		var body: some View {
+			DatePicker(
+				"",
+				selection: $selectedDate,
+				/*in: ...Date(),*/
+				displayedComponents: .hourAndMinute
+			)
+			.environment(\.locale, .init(identifier: "us"))
+			
+		}
+	}
+	
+}
+
+struct TestDatePickerMinuteInterval_Previews: PreviewProvider {
+	static var previews: some View {
+		TestDatePickerMinuteInterval()
+	}
+}
