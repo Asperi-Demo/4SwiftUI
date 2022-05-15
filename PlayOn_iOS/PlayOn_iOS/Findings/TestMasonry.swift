79c8869b0c2f616fdc1a1d197a9ea50f8f1b8d81
Asperi <asperi.2791@gmail.com>
2022-05-15 09:01:33 +0300

    Some more testing on Masonry

--- PlayOn_iOS/PlayOn_iOS/Findings/TestMasonry.swift
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestMasonry.swift
@@ -10,22 +10,30 @@ struct TestMasonry: View {
 	let wSpacing = CGFloat(4)
 	let hSpacing = CGFloat(4)
 	let columns = 3
+
 	struct Item: Identifiable {
 		let id = UUID()
 		let height: CGFloat
 	}
+
 	let items: [Item] = (0...99).map { _ in Item(height: CGFloat([43, 87, 121, 204].randomElement()!)) }
+
 	var body: some View {
 		GeometryReader { gp in
 			ScrollView {
-				HStack(alignment: .top, spacing: wSpacing) {
+				HStack(alignment: .top, spacing: wSpacing) { // Lazy not needed and gives incorrect layout
 					let sub = items.count / columns
 					ForEach(1..<columns+1, id: \.self) { i in
 						LazyVStack(spacing: hSpacing) {
-							ForEach(calculatedItems(i, sub)) {
+							ForEach(calculatedItems(i, sub)) { item in
 								Rectangle().stroke(.red, lineWidth: 2)
-									.frame(height: $0.height)
+									.frame(height: item.height)
 									.background(.green.opacity(0.3))
+									.onAppear {
+										// confirms appear on-scroll !!
+										print(">> \(i):\(item.id)")
+									}
+
 							}
 						}
 						.frame(width: (gp.size.width - wSpacing * CGFloat(columns - 1))/3)
