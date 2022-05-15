e937d54c5edc67cd219875f8caacb84a192678f3
Asperi <asperi.2791@gmail.com>
2022-05-15 08:37:49 +0300

    Some play with black-red design :)

--- PlayOn_iOS/PlayOn_iOS/Findings/TestTabBarShadow.swift
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestTabBarShadow.swift
@@ -9,9 +9,9 @@ import SwiftUI
 struct TestTabBarShadow: View {
 	func setupTabBar() {
 		let appearance = UITabBarAppearance()
-		appearance.shadowColor = .white
+		appearance.shadowColor = .red
 		appearance.shadowImage = UIImage(named: "tab-shadow")?.withRenderingMode(.alwaysTemplate)
-		appearance.backgroundColor = UIColor.darkGray
+		appearance.backgroundColor = UIColor.black
 		UITabBar.appearance().scrollEdgeAppearance = appearance
 	}
 
@@ -20,29 +20,44 @@ struct TestTabBarShadow: View {
 	}
 
 	var body: some View {
-		TabView {
-			Text("Markets").font(.title)
-				.tag(1)
-				.tabItem {
-					Image(systemName: "chart.xyaxis.line")
-					Text("Market")
-				}
+		ZStack(alignment: .bottom) {
+			TabView {
+				Text("Markets").font(.title)
+					.tag(1)
+					.tabItem {
+						Image(systemName: "chart.xyaxis.line")
+						Text("Market")
+					}
 
-			Text("PortfolioTabBarView")
-				.tag(2)
-				.tabItem {
-					Image(systemName: "bitcoinsign.square.fill")
-					Text("Portfolio")
-				}
+				Text("PortfolioTabBarView")
+					.tag(2)
+//					.tabItem {
+//						Image(systemName: "bitcoinsign.square.fill")
+//						Text("Portfolio")
+//					}
 
-			Text("SettingsTabBarView")
-				.tag(3)
-				.tabItem {
-					Image(systemName: "gearshape.fill")
-					Text("Settings")
+				Text("SettingsTabBarView")
+					.tag(3)
+					.tabItem {
+						Image(systemName: "gearshape.fill")
+						Text("Settings")
+					}
+			}
+			Circle().fill(.background)
+				.frame(width: 80, height: 80)
+				.overlay(GeometryReader { gp in
+					Color.clear.overlay(
+						Image(systemName: "bitcoinsign.square")
+							.resizable().foregroundColor(.secondary)
+							.frame(width: gp.size.width * 0.4, height: gp.size.height * 0.4)
+					)
+				})
+				.compositingGroup()
+				.shadow(color: .red.opacity(0.5), radius: 6, x: 0, y: 0)
+				.onTapGesture {
+					// manipulate with TabView selection here !!
 				}
 		}
-
 	}
 }
 
