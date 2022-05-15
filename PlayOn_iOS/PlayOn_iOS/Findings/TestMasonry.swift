3123d3d53743f64e87ba92a45be5ad741f7c081c
Asperi <asperi.2791@gmail.com>
2022-05-15 08:54:54 +0300

    First try for masonry layout

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestMasonry.swift
@@ -0,0 +1,48 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestMasonry: View {
+	let wSpacing = CGFloat(4)
+	let hSpacing = CGFloat(4)
+	let columns = 3
+	struct Item: Identifiable {
+		let id = UUID()
+		let height: CGFloat
+	}
+	let items: [Item] = (0...99).map { _ in Item(height: CGFloat([43, 87, 121, 204].randomElement()!)) }
+	var body: some View {
+		GeometryReader { gp in
+			ScrollView {
+				HStack(alignment: .top, spacing: wSpacing) {
+					let sub = items.count / columns
+					ForEach(1..<columns+1, id: \.self) { i in
+						LazyVStack(spacing: hSpacing) {
+							ForEach(calculatedItems(i, sub)) {
+								Rectangle().stroke(.red, lineWidth: 2)
+									.frame(height: $0.height)
+									.background(.green.opacity(0.3))
+							}
+						}
+						.frame(width: (gp.size.width - wSpacing * CGFloat(columns - 1))/3)
+					}
+				}
+			}
+		}
+	}
+
+	func calculatedItems(_ i: Int, _ sub: Int) -> ArraySlice<Item> {
+		items[(i-1)*sub..<(i*sub > items.count ? items.count : i*sub)]
+	}
+}
+
+
+struct TestMasonry_Previews: PreviewProvider {
+    static var previews: some View {
+        TestMasonry()
+    }
+}
