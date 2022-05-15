c06223ad45768e46a860214f637a58444af9998b
Asperi <asperi.2791@gmail.com>
2022-05-15 15:18:51 +0300

    Test of TabView fit to content height

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TabViewDynamicHeight.swift
@@ -0,0 +1,44 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TabViewDynamicHeight: View {
+	@State private var selection: Int = 0
+	@State private var rect: CGRect = .zero
+	var body: some View {
+		VStack {
+			TabView(selection: $selection) {
+				ForEach(0..<5, id: \.self) { index in
+					VStack {
+						Text("Text \(index)")
+						Text("Text \(index)")
+						Text("Text \(index)")
+					}
+					.frame(maxWidth: .infinity)
+					.background(GeometryReader {
+						Color.clear.preference(key: ViewRectKey.self,
+													  value: [$0.frame(in: .local)])
+					})
+					.tag(index)
+				}
+			}
+			.tabViewStyle(PageTabViewStyle())
+			.frame(height: rect.size.height
+					 + 60 /* just to avoid page indicator overlap */)
+			.background(Color.red)
+			.onPreferenceChange(ViewRectKey.self) { rects in
+				self.rect = rects.first ?? .zero
+			}
+		}
+	}
+}
+
+struct TabViewDynamicHeight_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewDynamicHeight()
+    }
+}
