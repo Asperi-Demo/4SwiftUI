04a7841ba1483f30f782472f4fb1ff11bfa8f9a2
Asperi <asperi.2791@gmail.com>
2022-05-18 18:18:36 +0300

    Test access/disabling bouncing for ScrollView

---
+++ PlayOn_iOS/PlayOn_iOS/Extensions/UIView+.swift
@@ -0,0 +1,21 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import UIKit
+
+extension UIView {
+	/// Find parent scroll view if there are any (replication of `NSView.enclosingScrollView`)
+	func enclosingScrollView() -> UIScrollView? {
+		var next: UIView? = self
+		repeat {
+			next = next?.superview
+			if let scrollview = next as? UIScrollView {
+				return scrollview
+			}
+		} while next != nil
+		return nil
+	}
+}
---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestScrollViewBouncing.swift
@@ -0,0 +1,48 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestScrollViewBouncing: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		var body: some View {
+			GeometryReader { geo in
+				ScrollView {
+					Rectangle()
+						.frame(width: geo.size.width, height: 1800)
+						.foregroundColor(.black)
+						.background(ScrollViewConfigurator {
+							$0?.bounces = false               // << here !!
+						})
+					Spacer()
+				}
+			}
+		}
+	}
+
+	struct ScrollViewConfigurator: UIViewRepresentable {
+		let configure: (UIScrollView?) -> ()
+		func makeUIView(context: Context) -> UIView {
+			let view = UIView()
+			DispatchQueue.main.async {
+				configure(view.enclosingScrollView())
+			}
+			return view
+		}
+
+		func updateUIView(_ uiView: UIView, context: Context) {}
+	}
+}
+
+struct TestScrollViewBouncing_Previews: PreviewProvider {
+	static var previews: some View {
+		TestScrollViewBouncing()
+	}
+}
