b627a7459315c7b90b3d25fe9cdc9cdeb75e2e76
Asperi <asperi.2791@gmail.com>
2022-05-18 20:44:23 +0300

    Tested TextField move focus

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestTextFieldMoveFocus.swift
@@ -0,0 +1,40 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Grybov, Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestTextFieldMoveFocus: View {
+	@State private var firstname = ""
+	@State private var lastname = ""
+
+	@FocusState private var infocus: Field?
+
+	enum Field {
+		case first, last
+	}
+
+	var body: some View {
+		VStack {
+			NavigationView {
+				Form {
+					TextField("First name", text: $firstname, onCommit: { infocus = .last })
+						.focused($infocus, equals: .first)
+
+					TextField("Last name", text: $lastname)
+						.focused($infocus, equals: .last)
+
+				}
+				.navigationBarTitle("Add a Person", displayMode: .inline)
+			}
+		}
+	}
+}
+
+struct TestTextFieldMoveFocus_Previews: PreviewProvider {
+    static var previews: some View {
+        TestTextFieldMoveFocus()
+    }
+}
