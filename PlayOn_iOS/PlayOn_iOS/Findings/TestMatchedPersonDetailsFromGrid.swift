42e676a85fe45804828bba6c51282ca2864dcb17
Asperi <asperi.2791@gmail.com>
2022-05-23 22:08:12 +0300

    Test for some person details matching effect

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestMatchedPersonDetailsFromGrid.swift
@@ -0,0 +1,101 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestMatchedPersonDetailsFromGrid: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct PersonDetails: View {
+		@State var person:Person
+
+		var body: some View {
+			ZStack {
+
+				Color.yellow
+
+				HStack {
+					Text("PERSON")
+					Spacer()
+				}
+
+				Text(person.name)
+
+				Spacer()
+
+			}
+			.border(.red)
+		}
+	}
+
+	struct Person: Identifiable, Equatable {
+		let id = UUID()
+		let name: String
+	}
+
+	struct PersonView: View {
+		let person: Person
+		var body: some View {
+			ZStack {
+				Color.blue
+				Text(person.name)
+					.foregroundColor(.white)
+			}
+		}
+	}
+
+	struct ContentView: View {
+
+		@Namespace private var gridSpace
+
+		@State var persons = [Person(name: "A"), Person(name: "B"), Person(name: "C")]
+		@State var selectedPerson:Person?
+
+		let c = GridItem(.adaptive(minimum: 200, maximum: 400), spacing: 20)
+
+		var body: some View {
+
+			ZStack {
+				ScrollView {
+					LazyVGrid(columns: [c]) {
+						ForEach(persons) { person in
+							PersonView(person: person)   // << just to keep same in place
+								.overlay(
+									PersonView(person: person)
+										.matchedGeometryEffect(id: person.id, in: gridSpace)
+										.onTapGesture {
+											if nil == selectedPerson {
+												selectedPerson = person
+											}
+										})
+								.frame(width: 100, height: 100)
+						}
+					}
+				}
+				VStack {   // << to remove fluently
+					if let person = selectedPerson {
+						PersonDetails(person: person)
+							.matchedGeometryEffect(id: person.id, in: gridSpace)
+							.transition(.scale(scale: 1))
+							.frame(width: 360, height: 360)
+							.onTapGesture {
+								selectedPerson = nil
+							}
+					}
+				}
+			}
+			.animation(.default, value: selectedPerson)
+		}
+	}
+}
+
+struct TestMatchedPersonDetailsFromGrid_Previews: PreviewProvider {
+	static var previews: some View {
+		TestMatchedPersonDetailsFromGrid()
+	}
+}
