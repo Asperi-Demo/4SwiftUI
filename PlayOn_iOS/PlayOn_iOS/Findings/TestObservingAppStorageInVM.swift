f40f8858b7ae93a9c575ed41aa3cc9f9dcdb1cc1
Asperi <asperi.2791@gmail.com>
2022-05-23 18:52:42 +0300

    Test on declaring AppStorage in VM and observing it

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestObservingAppStorageInVM.swift
@@ -0,0 +1,62 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+import Combine
+
+struct TestObservingAppStorageInVM: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		@StateObject var vm = ValueViewModel()
+		@AppStorage("MyValue") var myValue = 0
+
+		var body: some View {
+			VStack {
+				Text("Title: " + vm.title)
+				Button("Generage") {
+					myValue = Int.random(in: 1...100)
+				}
+				.onChange(of: vm.foo) {
+					print("Change >> \($0)")
+				}
+				.onReceive(vm.$foo) {
+					print("Event: \($0)")
+				}
+			}
+		}
+	}
+
+	class AppSettings: ObservableObject {
+		@AppStorage("MyValue") var myValue = 0
+		@AppStorage("foo") var foo = "default"
+	}
+
+	class ValueViewModel: ObservableObject {
+		private var settings = AppSettings()
+
+		@Published var title: String = ""
+		@Published var foo: String = "default" { didSet { print(foo) } }
+
+		private var cancellable: AnyCancellable?
+
+		init() {
+			cancellable = settings.objectWillChange.sink { [weak self] _ in
+				guard let self = self else { return }
+				self.title = "Hello \(self.settings.myValue)"
+				self.foo = self.settings.foo
+			}
+		}
+	}
+}
+
+struct TestObservingAppStorageInVM_Previews: PreviewProvider {
+	static var previews: some View {
+		TestObservingAppStorageInVM()
+	}
+}
