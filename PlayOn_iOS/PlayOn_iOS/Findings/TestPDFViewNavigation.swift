00e1fa0eeda8e037b47b170392d0b21390af7851
Asperi <asperi.2791@gmail.com>
2022-05-24 14:55:34 +0300

    Test programmatic navigation in PDFView representable

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestPDFViewNavigation.swift
@@ -0,0 +1,78 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+import PDFKit
+
+struct TestPDFViewNavigation: View {
+	var body: some View {
+		PDFKitView(url: Bundle.main.url(forResource: "document", withExtension: "pdf")!)
+	}
+
+	struct PDFKitView: View {
+		var url: URL
+
+		@State var currentPage: Int = 0
+		@State var total: Int = 0
+
+		var body: some View {
+			VStack {
+				HStack {
+					Button("<") { currentPage -= 1 }.padding()
+						.disabled(currentPage == 0)
+					Button(">") { currentPage += 1 }.padding()
+						.disabled(currentPage == total - 1)
+				}
+				PDFKitRepresentedView(url, $currentPage, $total)
+			}
+		}
+	}
+
+	struct PDFKitRepresentedView: UIViewRepresentable {
+		let url: URL
+		@Binding var currentPage: Int
+		@Binding var total: Int
+
+		init(_ url: URL, _ currentPage: Binding<Int>, _ total: Binding<Int>) {
+			self.url = url
+			self._currentPage = currentPage
+			self._total = total
+		}
+
+		func makeUIView(context: Context) -> UIView {
+			guard let document = PDFDocument(url: self.url) else { return UIView() }
+
+			let pdfView = PDFView()
+			print("PDFVIEW IS CREATED")
+			pdfView.document = document
+			pdfView.displayMode = .singlePage
+			pdfView.displayDirection = .horizontal
+			pdfView.autoScales = true
+			pdfView.usePageViewController(true)
+
+			DispatchQueue.main.async {
+				self.total = document.pageCount
+				print("Total pages: \(total)")
+			}
+			return pdfView
+		}
+
+		func updateUIView(_ uiView: UIView, context: Context) {
+			guard let pdfView = uiView as? PDFView else { return }
+
+			if currentPage < total {
+				pdfView.go(to: pdfView.document!.page(at: currentPage)!)
+			}
+		}
+
+	}
+}
+
+struct TestPDFViewNavigation_Previews: PreviewProvider {
+	static var previews: some View {
+		TestPDFViewNavigation()
+	}
+}
