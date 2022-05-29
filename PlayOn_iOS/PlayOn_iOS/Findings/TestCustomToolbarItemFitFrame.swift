a7851d9202e13eabd361b486fefd47c3a5da312f
Asperi <asperi.2791@gmail.com>
2022-05-29 12:55:14 +0300

    Test custom toolbar button item to fit in frame

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestCustomToolbarItemFitFrame.swift
@@ -0,0 +1,67 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestCustomToolbarItemFitFrame: View {
+    var body: some View {
+        ContentView()
+    }
+
+    struct ContentView: View {
+        let size = 45.0
+        let fontSize = 17.0
+        
+        var body: some View {
+            NavigationView {
+                Button { } label: {
+                    Image(systemName: "globe")
+                        .resizable()
+                        .scaledToFill()
+                }
+                .frame(width: size, height: size)
+                .border(.yellow)
+                .toolbar {
+                    HStack(alignment: .top) {
+                        
+                        Button { print(">> action here")} label: {
+                            Image(systemName: "globe")
+                                .resizable()
+                                .scaledToFill()
+                        }
+                        .buttonStyle(MyTabbarButtonStyle())
+                        .frame(width: size, height: size)
+                        .border(.red)
+                        
+                        // Image and Title
+                        VStack {
+                            Image("nemo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: size)
+                            Text("Navigation Title")
+                                .font(.system(size: fontSize))
+                                .bold()
+                        }.border(.green)
+                    }
+                }
+            }
+        }
+    }
+
+    struct MyTabbarButtonStyle: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .foregroundColor(configuration.isPressed ? .gray : .blue)
+        }
+    }
+}
+
+struct TestCustomToolbarItemFitFrame_Previews: PreviewProvider {
+    static var previews: some View {
+        TestCustomToolbarItemFitFrame()
+    }
+}
