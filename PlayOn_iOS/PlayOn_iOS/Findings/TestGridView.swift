95a09c0329e1fab8411e1dd4682c8daef9105b51
Asperi <asperi.2791@gmail.com>
2022-06-09 16:15:47 +0300

    some fixes

--- PlayOn_iOS/PlayOn_iOS/Findings/TestGridView.swift
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestGridView.swift
@@ -6,52 +6,54 @@
 
 import SwiftUI
 
+#if canImport(Charts)        // to block Xcode 13+ compiler error on Grid
 @available(iOS 16.0, *)
 struct TestGridView: View {
-    
-    var body: some View {
-        ProtoGrid()
-    }
-    
-    struct ProtoGrid: View {
-        
-        @State var index1 = 15.0
-        @State var index2 = 5.0
-        @State var index3 = 225.0
-        
-        @State private var imageWidth = CGFloat.zero
-        
-        var body: some View {
-            VStack {
-                Rectangle()
-                    .fill(Color.red)
-                Grid {
-                    GridRow {
-                        Image(systemName: "person").imageScale(.large)
-                        Slider(value: $index1, in: 0...10)
-                        Text("\(String(format: "%.2f", index1))").font(Font.system(.body).monospacedDigit())
-                    }
-                    GridRow {
-                        Image(systemName: "megaphone").imageScale(.large)
-                        Slider(value: $index2, in: 0...10)
-                        Text("\(String(format: "%.2f", index2))").font(Font.system(.body).monospacedDigit())
-                    }
-                    GridRow {
-                        Image(systemName: "ruler").imageScale(.large)
-                        Slider(value: $index3, in: 0...10)
-                        Text("\(String(format: "%.2f", index3))").font(Font.system(.body).monospacedDigit())
-                            .gridColumnAlignment(.trailing)
-                    }
-                }
-            }
-            .padding()
-        }
-    }
+
+	var body: some View {
+		ProtoGrid()
+	}
+
+	struct ProtoGrid: View {
+		@State var index1 = 15.0
+		@State var index2 = 5.0
+		@State var index3 = 225.0
+
+		@State private var imageWidth = CGFloat.zero
+
+		var body: some View {
+			VStack {
+				Rectangle()
+					.fill(Color.red)
+
+				Grid {
+					GridRow {
+						Image(systemName: "person").imageScale(.large)
+						Slider(value: $index1, in: 0...10)
+						Text("\(String(format: "%.2f", index1))").font(Font.system(.body).monospacedDigit())
+					}
+					GridRow {
+						Image(systemName: "megaphone").imageScale(.large)
+						Slider(value: $index2, in: 0...10)
+						Text("\(String(format: "%.2f", index2))").font(Font.system(.body).monospacedDigit())
+					}
+					GridRow {
+						Image(systemName: "ruler").imageScale(.large)
+						Slider(value: $index3, in: 0...10)
+						Text("\(String(format: "%.2f", index3))").font(Font.system(.body).monospacedDigit())
+							.gridColumnAlignment(.trailing)
+					}
+				}
+			}
+			.padding()
+		}
+	}
 }
 
 @available(iOS 16.0, *)
 struct TestGridView_Previews: PreviewProvider {
-    static var previews: some View {
-        TestGridView()
-    }
+	static var previews: some View {
+		TestGridView()
+	}
 }
+#endif
