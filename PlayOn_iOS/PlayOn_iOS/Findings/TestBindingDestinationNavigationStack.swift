9d766c8406c5911acf52f1114b5ea85443ace6d1
Asperi <asperi.2791@gmail.com>
2022-06-11 16:21:06 +0300

    Test for NavigationStack and Binding as destination value

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestBindingDestinationNavigationStack.swift
@@ -0,0 +1,68 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestBindingDestinationNavigationStack: View {
+    var body: some View {
+        if #available(iOS 16.0, *) {
+            ParentListView()
+        } else {
+            EmptyView()
+        }
+    }
+    
+    @available (iOS 16.0, *)
+    struct ParentListView: View {
+        @State private var numbers = [1, 2, 3, 4, 5]
+        var body: some View {
+            NavigationStack {
+                List {
+                    ForEach($numbers) { number in
+                        NavigationLink(value: number) {
+                            Text("\(number.wrappedValue)")
+                        }
+                    }
+                }
+                .navigationDestination(for: Binding<Int>.self) { number in
+                    ChildDetailView(number: number)
+                }
+            }
+        }
+    }
+    
+    struct ChildDetailView: View {
+        @Binding var number: Int
+        var body: some View {
+            VStack {
+                Text("\(number)")
+                Button {
+                    number += 10
+                } label: {
+                    Text("Add 10")
+                }
+            }
+        }
+    }
+}
+
+extension Binding: Equatable where Value: Equatable {
+    public static func == (lhs: Binding<Value>, rhs: Binding<Value>) -> Bool {
+        return lhs.wrappedValue == rhs.wrappedValue
+    }
+}
+
+extension Binding: Hashable where Value: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        self.wrappedValue.hash(into: &hasher)
+    }
+}
+
+struct TestBindingDestinationNavigationStack_Previews: PreviewProvider {
+    static var previews: some View {
+        TestBindingDestinationNavigationStack()
+    }
+}
