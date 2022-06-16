b2e75c3a933ca5b4ea611571b2e2234ed7abf8c3
Asperi <asperi.2791@gmail.com>
2022-06-16 09:25:27 +0300

    Test fill AsyncImage in grid

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestFillAsyncImagesInGrid.swift
@@ -0,0 +1,76 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestFillAsyncImagesInGrid: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		@State private var results = [
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+			"https://source.unsplash.com/1600x900/?nature&\(Int.random(in: 1..<6236))",
+		]
+
+		let columns = [
+			GridItem(.flexible(), spacing: 1),
+			GridItem(.flexible(), spacing: 1),
+			GridItem(.flexible(), spacing: 1),
+		]
+		@State private var selected: Image? = nil
+		@State private var selectedID: String? = nil
+		@Namespace var ns
+		var body: some View {
+			ZStack {
+				ScrollView {
+					LazyVGrid(columns: columns, spacing: 1) {
+						ForEach(results, id: \.self) { result in
+							AsyncImage(url: URL(string: result)) { phase in
+								switch phase {
+								case .success(let image):
+									image
+										.resizable()
+										.scaledToFill()
+										.onTapGesture {
+											selectedID = result
+											selected = image
+										}
+								default:
+									Image(systemName: "car")
+								}
+							}
+							.frame(maxWidth: .infinity)
+						}
+					}
+				}
+				if let image = selected {
+					image
+						.resizable()
+						.aspectRatio(contentMode: .fill)
+						.onTapGesture {
+							selectedID = nil
+							selected = nil
+						}
+						.zIndex(1)
+				}
+			}.animation(.default, value: selected)
+		}
+	}
+}
+
+struct TestFillAsyncImagesInGrid_Previews: PreviewProvider {
+	static var previews: some View {
+		TestFillAsyncImagesInGrid()
+	}
+}
