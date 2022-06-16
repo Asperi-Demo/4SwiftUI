567d98b9001ebaffab329e1a127c2b96f8391828
Asperi <asperi.2791@gmail.com>
2022-06-16 21:07:08 +0300

    Test for List background on iOS 16

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestListBackgroundiOS16.swift
@@ -0,0 +1,44 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestListBackgroundiOS16: View {
+    var body: some View {
+        ContentView()
+    }
+    
+    struct ContentView: View {
+        init() {
+            UICollectionView.appearance().backgroundColor = .clear
+        }
+        var body: some View {
+            ZStack {
+                Color.red
+                List {
+                    Text("Item 1")
+                    Text("Item 2")
+                    Text("Item 3")
+                }
+            }
+        }
+    }
+}
+
+#if canImport(Charts)
+extension UICollectionReusableView {
+    override open var backgroundColor: UIColor? {
+        get { .clear }
+        set { }
+    }
+}
+#endif
+
+struct TestListBackgroundiOS16_Previews: PreviewProvider {
+    static var previews: some View {
+        TestListBackgroundiOS16()
+    }
+}
