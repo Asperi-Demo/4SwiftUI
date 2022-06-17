9db849f0419b2a0be24422d5139a7a633db4e1c3
Asperi <asperi.2791@gmail.com>
2022-06-17 16:31:08 +0300

    Tested navigation for invisible LazyVGrid views

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestNavigateLazyGridView.swift
@@ -0,0 +1,64 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestNavigateLazyGridView: View {
+	struct Item: Identifiable {
+		var id: Int { value }
+		var value: Int
+	}
+	@State var selectedItem: Item?
+
+	private let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+	private let items = (0..<100).map { Item(value: $0) }
+
+	var isActive: Binding<Bool> {
+		Binding(get: {selectedItem != nil}, set: {_ in selectedItem = nil})
+	}
+
+	var body: some View {
+		NavigationView {
+			ScrollView {
+				LazyVGrid(columns: columns) {
+					ForEach(items) { item in
+						RoundedRectangle(cornerRadius: 16).fill(.yellow)
+							.frame(maxWidth: .infinity).aspectRatio(1, contentMode: .fit)
+							.overlay(Text("Item \(item.value)"))
+							.onTapGesture {
+								selectedItem = item
+							}
+					}
+				}
+			}
+			.padding(.horizontal)
+			.background(
+				NavigationLink(destination: DetailView(item: selectedItem), isActive: isActive) {
+					EmptyView()
+				}
+			)
+			.toolbar {
+				Button("Random") { selectedItem = items.randomElement() }
+			}
+		}
+	}
+
+	struct DetailView: View {
+		let item: Item?
+		var body: some View {
+			if let item = item {
+				Text("Details for: \(item.value)")
+			}
+		}
+	}
+}
+
+
+struct TestNavigateLazyGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestNavigateLazyGridView()
+    }
+}
