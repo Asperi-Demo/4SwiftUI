7f088ce53d4f2c535cc994ae5ee97e7a92277712
Asperi <asperi.2791@gmail.com>
2022-06-18 21:24:41 +0300

    Test menu sort direction state change

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestMenuSortOrderChange.swift
@@ -0,0 +1,58 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestMenuSortOrderChange: View {
+	var body: some View {
+		ContentView()
+	}
+
+	struct ContentView: View {
+		@State private var selectedSort: SortOption = .name
+		@State private var isSortAscending = true
+
+		enum SortOption: String, CaseIterable, Identifiable {
+			var id: Self { self }
+			case name = "Name"
+			case number = "Number"
+			case length = "Length"
+		}
+
+		var sorting: Binding<SortOption> { .init(
+			get: { self.selectedSort },
+			set: {
+				if self.selectedSort == $0 {
+					self.isSortAscending.toggle()
+				}
+				self.selectedSort = $0
+			}
+		)}
+
+		var body: some View {
+			Menu("Sort") {
+				Picker(selection: sorting, label: Text("Sorting options")) {
+					ForEach(SortOption.allCases) { option in
+						HStack {
+							Text(option.rawValue)
+							Spacer()
+							if selectedSort == option {
+								Image(systemName: isSortAscending ? "chevron.down" : "chevron.up")
+							}
+						}
+						.tag(option)
+					}
+				}
+			}
+		}
+	}
+}
+
+struct TestMenuSortOrderChange_Previews: PreviewProvider {
+	static var previews: some View {
+		TestMenuSortOrderChange()
+	}
+}
