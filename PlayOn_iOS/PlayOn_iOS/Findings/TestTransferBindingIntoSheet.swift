e3eb94c7b90b7532d7175fa6ba39badfc61fd0e0
Asperi <62171579+Asperi-Demo@users.noreply.github.com>
2022-06-21 12:07:46 +0300

    Update TestTransferBindingIntoSheet.swift

--- PlayOn_iOS/PlayOn_iOS/Findings/TestTransferBindingIntoSheet.swift
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestTransferBindingIntoSheet.swift
@@ -1,3 +1,9 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
 import SwiftUI
 
 struct TestTransferBindingIntoSheet: View {
