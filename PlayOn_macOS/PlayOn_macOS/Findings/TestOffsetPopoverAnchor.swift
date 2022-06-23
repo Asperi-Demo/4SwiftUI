8fd6578c2f0fe83427c9f918466e23609ebca966
Asperi <asperi.2791@gmail.com>
2022-06-23 16:59:47 +0300

    Test offset popover anchor on control (e.g. Button) from predefined one

---
+++ PlayOn_macOS/PlayOn_macOS/Findings/TestOffsetPopoverAnchor.swift
@@ -0,0 +1,40 @@
+// BOYCOTT on russia! Don't buy, sell, support - HELP TO STOP WAR!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION: 	By using this you agree do not repost any part of this code
+//					on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestOffsetPopoverAnchor: View {
+    @State private var isPop = false
+    @State private var text = ""
+
+    var body: some View {
+        VStack{
+            Button { self.isPop.toggle() }
+            	label: { Text("Pop").padding() }
+            	.buttonStyle(.plain)
+				.border(.blue)
+				.padding(.horizontal, -8)  // << here !!
+				.border(.yellow)
+                .popover(isPresented: $isPop,
+                         attachmentAnchor: .point(.bottomTrailing),
+                         arrowEdge: .bottom) {
+                    VStack { // just example
+                        Text("Test").padding(.top)
+                        TextField("Placeholder", text: self.$text)
+                            .padding(.horizontal)
+                            .padding(.bottom)
+                            .frame(width: 200)
+                    }
+            }
+        }.frame(width: 300, height: 200)
+    }
+}
+
+struct TestOffsetPopoverAnchor_Previews: PreviewProvider {
+    static var previews: some View {
+        TestOffsetPopoverAnchor()
+    }
+}
