8f7bf02078d4646b65ec09fcd4e727e014921f7d
Asperi <asperi.2791@gmail.com>
2022-07-05 06:57:35 +0300

    Test new swift 5.7 feature for `any` packing/unpacking with SwiftUI - failure!

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestAnyProtocolForViews.swift
@@ -0,0 +1,112 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+#if canImport(Charts)
+
+@available(iOS 16, *)
+struct TestAnyProtocolForViews_Previews: PreviewProvider {
+    static var previews: some View {
+        TestNavigatable()
+        TestConverter()
+    }
+}
+
+// !!! both cases below result in `any View` which generates compiler error:
+// >> Type 'any View' cannot conform to 'View'
+
+// MARK: - Test `any` with model for use in View
+@available(iOS 16, *)
+protocol Provider<Value> {
+    associatedtype Value
+    
+    func get() -> Value
+}
+
+@available(iOS 16, *)
+struct S {
+    var stringProvider: any Provider<String>
+}
+
+@available(iOS 16, *)
+struct ProviderView<P: Provider>: View {
+    let provider: P
+    
+    var body: some View {
+        Text(String(describing: type(of: provider.get())))
+    }
+}
+
+@available(iOS 16, *)
+struct DummyProvider: Provider {
+    typealias Value = String
+    
+    func get() -> String {
+        "Hello World!"
+    }
+}
+
+@available(iOS 16, *)
+struct TestConverter: View {
+    let s = S(stringProvider: DummyProvider())
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+//            converter(for: s.stringProvider) // Type 'any View' cannot conform to 'View'
+        }
+    }
+    
+    func converter(for provider: some Provider<String>) -> some View {
+        AnyView(ProviderView(provider: provider))
+    }
+}
+
+
+// MARK: - Test for direct dependency on `any View`
+protocol Navigatable: Identifiable {
+    associatedtype V: View
+    var id: Int { get }
+    var destination: V { get }
+}
+struct HomePageGridItem<Dest: View>: Navigatable {
+    var id: Int
+    var destination: Dest
+}
+
+@available(iOS 16, *)
+struct TestNavigatable: View {
+    let dest1 = HomePageGridItem(id: 1, destination: Text("Hello"))
+    let dest2 = HomePageGridItem(id: 2, destination: Image(systemName: "car"))
+
+    let items: [any Navigatable] = [
+        HomePageGridItem(id: 1, destination: Text("Hello")),
+        HomePageGridItem(id: 2, destination: Image(systemName: "car"))
+    ]
+
+    var body: some View {
+        HStack {
+            dest2.destination
+            dest1.destination
+//            destination(for: items[0]) // << Type 'any View' cannot conform to 'View'
+//            items[1].destination // Type 'any View' cannot conform to 'View'
+        }
+//        List {
+//            ForEach(0..<items.count, id: \.self) { i in
+//                self.destination(for: items[i])
+//            }
+//        }
+    }
+    
+    func destination(for i: some Navigatable) -> some View {
+        i.destination
+    }
+}
+#endif
