e6c9c502cc44e1cded7609eeca4ecb39e63a369b
Asperi <asperi.2791@gmail.com>
2022-07-07 18:12:50 +0300

    Fixed constants access

--- PlayOn_iOS/PlayOn_iOS/Helpers/Constants.swift
+++ PlayOn_iOS/PlayOn_iOS/Helpers/Constants.swift
@@ -8,11 +8,11 @@ import SwiftUI
 
 // Just different strings for demo and testing
 struct Lorem {
-	let short = "Lorem ipsum dolor sit amet"
+	static let short = "Lorem ipsum dolor sit amet"
 
-	let long = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..."
+	static let long = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..."
 
-	let original = """
+	static let original = """
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.
 	"""
 }
