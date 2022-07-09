04c55c94f580639fb3a612768e03e70ca6974e4e
Asperi <asperi.2791@gmail.com>
2022-07-09 22:38:19 +0300

    Playing with indicator control

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestIndicatorControlLines.swift
@@ -0,0 +1,63 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestIndicatorControlLines: View {
+	@State private var value = CGFloat.zero
+    var body: some View {
+		IndicatorView(current: value)
+			.contentShape(Rectangle())
+			.onTapGesture {
+				withAnimation {
+				value = CGFloat((0...1000).randomElement()!)
+				}
+			}
+			.padding(.horizontal)
+    }
+}
+
+struct IndicatorView: View {
+	var goal: CGFloat = 1000
+	var current: CGFloat = 0
+	var thick: CGFloat = 40
+
+	private let arrow = Image(systemName: "triangle.fill").resizable()
+
+	var body: some View {
+		ZStack {
+			Circle()
+				.trim(from: 0.5, to: 1)
+				.stroke(lineWidth: thick).foregroundColor(.yellow)
+			Circle()
+				.trim(from: 0.5, to: (current / goal) * 0.5 + 0.5)
+				.stroke(lineWidth: thick).foregroundColor(.blue)
+		}
+		.padding(20)
+		.aspectRatio(contentMode: .fit)
+		.overlay(GeometryReader { gp in
+			Color.clear.overlay(
+			arrow.frame(width: 28, height: 28)
+				.foregroundColor(.white)
+				.overlay(arrow.padding(6).foregroundColor(current == .zero ? .yellow : .blue))
+				.transformEffect(CGAffineTransform(translationX: 0, y: -gp.size.height / 2.0 + thick))
+				.rotationEffect(.radians(-CGFloat.pi / 2.0 + (current / goal) * CGFloat.pi))
+			)
+		})
+		.overlay(
+			Text("\(current, specifier: "%.f")").font(.largeTitle).bold()
+				.animation(.none, value: current)
+		)
+	}
+}
+
+
+
+struct TestIndicatorControlLines_Previews: PreviewProvider {
+    static var previews: some View {
+        TestIndicatorControlLines()
+    }
+}
