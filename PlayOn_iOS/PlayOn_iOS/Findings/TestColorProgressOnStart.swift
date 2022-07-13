65d460c5888324ad7f94433d0280b298899350fc
Asperi <asperi.2791@gmail.com>
2022-07-13 09:14:57 +0300

    Test color progress bar on start

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestColorProgressOnStart.swift
@@ -0,0 +1,49 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestColorProgressOnStart: View {
+	@State private var value = 0.8
+
+	var body: some View {
+		VStack {
+			Button("Next") {
+				value = [0.2, 0.8, 0.5, 0.7, 0.3, 0.6, 0.9].randomElement()!
+			}
+			ColorProgress(currentProgress: value)
+		}
+	}
+
+	struct ColorProgress: View {
+		@State private var fraction: CGFloat = 0
+		var currentProgress: CGFloat
+
+		var body: some View {
+			ZStack(alignment: .leading) {
+				RoundedRectangle(cornerRadius: 20)
+					.foregroundColor(.gray)
+					.frame(width: 300, height: 20, alignment: .center)
+				RoundedRectangle(cornerRadius: 20)
+					.foregroundColor(Color(red: min(2.0 - (fraction * 2.0), 1.0), green: min((fraction * 2.0), 1.0), blue: 0))
+					.frame(width: 300 * fraction, height: 20)
+			}
+			.animation(.easeInOut(duration: 1.0), value: fraction)
+			.onAppear {
+				fraction = currentProgress
+			}
+			.onChange(of: currentProgress) {
+				fraction = $0
+			}
+		}
+	}
+}
+
+struct TestColorProgressOnStart_Previews: PreviewProvider {
+	static var previews: some View {
+		TestColorProgressOnStart()
+	}
+}
