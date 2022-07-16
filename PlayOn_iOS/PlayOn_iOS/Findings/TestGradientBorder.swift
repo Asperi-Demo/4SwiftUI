664cb91506837f36fcbe55f34188bd1a0e1a9eba
Asperi <asperi.2791@gmail.com>
2022-07-16 23:05:40 +0300

    Test gradient border for view

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestGradientBorder.swift
@@ -0,0 +1,48 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestGradientBorder: View {
+    var specialRadialGradient: some View {
+        GeometryReader { geometry in
+            RadialGradient(
+                gradient: Gradient(
+                    stops: [
+                        .init(color: Color.yellow.opacity(0.5), location: 0),
+                        .init(color: Color.blue.opacity(0.5), location: 1)
+                    ]
+                ),
+                center: .init(x: 0.1432, y: 0.7254),
+                startRadius: .zero,
+                endRadius: geometry.size.width * 0.75
+            )
+            .background(Color.green)
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+//            Color.red   // uncomment to test transparency
+            let borderWidth = 2.0
+            Text("Abc")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(
+                    specialRadialGradient
+                        .overlay(Rectangle().padding(borderWidth)    // << here !!
+                            .blendMode(.destinationOut))
+                        .compositingGroup()
+                )
+        }
+    }
+}
+
+struct TestGradientBorder_Previews: PreviewProvider {
+    static var previews: some View {
+        TestGradientBorder()
+    }
+}
