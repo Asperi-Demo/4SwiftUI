5e6d8cf231b30bfb98320e7d5e6b768b3b7aaf4a
Asperi <asperi.2791@gmail.com>
2022-07-19 00:36:24 +0300

    Test OS conditional flows for backgroundTask

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/iOS16/TestBackgroundTaskWithLegacyOSSupport.swift
@@ -0,0 +1,63 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+#if false // disabled to avoid conflict with main App
+
+import SwiftUI
+import BackgroundTasks
+
+struct AppScene: Scene {    // Legacy app scene
+    @Environment(\.scenePhase) var scenePhase
+   
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+@available(iOS 16, *)
+struct RefreshingAppScene: Scene {   // New app scene
+    
+    @Environment(\.scenePhase) var scenePhase
+   
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .backgroundTask(.appRefresh("refreshAllData")) {
+            try? await Task.sleep(nanoseconds: 2_000_000) // just to be something for test
+        }
+    }
+}
+
+
+@main
+struct AppLauncher {
+    static func main() {
+        if #available(iOS 16, *) { // << OS condition here !!
+            New_iOSApp.main()
+        } else {
+            Old_iOSApp.main()
+        }
+    }
+}
+
+@available (iOS 16, *)
+struct New_iOSApp: App {   // New app
+
+    var body: some Scene {
+        RefreshingAppScene()
+    }
+}
+
+struct Old_iOSApp: App {   // Legacy app
+    var body: some Scene {
+        AppScene()
+    }
+}
+
+#endif
