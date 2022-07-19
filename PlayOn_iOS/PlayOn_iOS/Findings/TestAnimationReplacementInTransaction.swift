8f2286e0971f297bbcf958d66f1a835625cd4414
Asperi <asperi.2791@gmail.com>
2022-07-19 18:52:00 +0300

    Test animation substitution in transaction

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/TestAnimationReplacementInTransaction.swift
@@ -0,0 +1,53 @@
+// 🔥BOYCOTT on russia - terrorist must be punished!
+// «Русский военный корабль, иди на хуй!» (c) Ukrainian Frontier Guard
+//
+// ATTENTION:     By using this you agree do not repost any part of this code
+//                    on StackOverflow site. Thanks, Asperi.
+
+import SwiftUI
+
+struct TestAnimationReplacementInTransaction: View {
+    @State var show: Bool = false
+    @State var reverseDelay: Bool = false
+
+    @State var delay1 = 0.3
+    @State var delay2 = 0.5
+    var body: some View {
+        VStack {
+            Button(action:{
+            	withAnimation {
+                	self.show.toggle()
+                }
+            }) {
+                Text("Animate")
+                    .font(.largeTitle)
+            }
+            if show {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(height: 100)
+                    .transition(.move(edge: .trailing))
+                    .transaction {
+                    	$0.animation = Animation.spring().delay(delay1)
+					}
+                    .onAppear { self.delay1 = 0.5 }
+                    .onDisappear { self.delay1 = 0.3 }
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(height: 100)
+                    .transition(.move(edge: .trailing))
+                    .transaction {
+                    	$0.animation = Animation.spring().delay(delay2)
+					}
+                    .onAppear { self.delay2 = 0.3 }
+                    .onDisappear { self.delay2 = 0.5 }
+            }
+        }
+    }
+}
+
+struct TestAnimationReplacementInTransaction_Previews: PreviewProvider {
+    static var previews: some View {
+        TestAnimationReplacementInTransaction()
+    }
+}
